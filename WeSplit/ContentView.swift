06d4b6d33350f7ebd1d95738d1f356d2bf3a0bb3
Orlando Vargas <Orlandov87@live.com>
2021-10-06 01:26:56 -0400

    Update ContentView.swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var checkAmount = "0.00"
+    @State var numberOfPeople = 1
+    @State var tipPercentage = 1;
+    @State var tipPercentages: [Int] = [5, 10, 15, 20]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
