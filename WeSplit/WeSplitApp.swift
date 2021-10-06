08a782c54a4cae4e3b93ef655e0dcd685c495ceb
Orlando Vargas <Orlandov87@live.com>
2021-10-06 00:43:47 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Orlando Vargas on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
